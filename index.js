4f1bf4fa44611da82d7a6bf16c66529711948edf
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-01-22 12:16:44 +0530

    commit: Update mongoose and insert datain mongo

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-let mongoose = require("moongoose");
+let mongoose = require("mongoose");
 
 
 //connection
 mongoose.connect("mongodb://localhost/HHD")
     .then(() => console.log(`connected to db`))
     .catch((error) => console.log(`something went wrong ${error.message}`));
+
+
+let courseSchema = new mongoose.Schema({
+    author: { type: String, required: true },
+    price: { type: Number, required: true },
+    course: [String],
+    date: { type: Date, default: Date.now() },
+    isPublished: { type: Boolean, required: true }
+});
+
+
+let courseModel = mongoose.model("courses", courseSchema);
+
+async function Course() {
+    let authorcourse = new courseModel({
+        author: "Harshal",
+        price: 250,
+        course: ["React", "Node JS"],
+        isPublished: true
+
+    })
+    let data = await authorcourse.save();
+    console.log(data);
+}
+Course();
