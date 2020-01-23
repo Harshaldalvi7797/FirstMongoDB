86c36fa29a9c54948d42e719cd78fcf7e479f1b2
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-01-23 11:14:17 +0530

    comiit: update gitignire

--- index2.js
+++ index2.js
@@ -1,6 +1,22 @@
 let mongoose = require("mongoose");
 
 //Connection
-mongoose.connect("mongodb://localhost/HHD")
+mongoose.connect("mongodb://localhost/HHD", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log(`Connected`))
     .catch((error) => console.log(`something went wrong ${error}`))
+
+
+let authorSchema = new mongoose.Schema({
+    author: { name: String, required: true },
+    website: { name: String, required: true },
+    isPublished: { name: String, required: true }
+
+})
+
+let courseSchema = new mongoose.Schema({
+    course: { name: String, required: true }
+
+})
+
+let authorsModel = mongoose.model("authors", authorSchema);
+let courseModel = mongoose.model("course", courseSchema);
